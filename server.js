c4458747d99c1d7205379f8d2f0b34540430ee29
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-17 12:04:03 +0100

    add front-end files & install packages

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-const seatsRouter = require('./routes/seats.routes');
 const concertsRouter = require('./routes/concerts.routes');
+const seatsRouter = require('./routes/seats.routes');
 const testimonialsRouter = require('./routes/testimonials.routes');
 const notFoundRouter = require('./routes/404.routes');
 
 const app = express();
 const port = 8000;
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/concerts', concertsRouter);
 app.use('/api/seats', seatsRouter);
 app.use('/api/testimonials', testimonialsRouter);
+
 app.use(notFoundRouter);
 
 app.listen(port, () => {
