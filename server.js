2e02b4940609a554487a2b591cb08db96509a716
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-22 21:05:00 +0100

    add Socket.IO & use

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const concertsRouter = require('./routes/concerts.routes');
 const seatsRouter = require('./routes/seats.routes');
 const testimonialsRouter = require('./routes/testimonials.routes');
 const notFoundRouter = require('./routes/404.routes');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
-const port = 8000;
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const port = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '/client/build')));
@@ -23,11 +26,17 @@ app.use('/api/testimonials', testimonialsRouter);
 
 // Return the main index.html file for all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.use(notFoundRouter);
 
-app.listen(process.env.PORT || port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+// Socket.IO connection listener
+io.on('connection', (socket) => {
+   console.log('New socket!');
+});
+
+// Start the server
+server.listen(port, () => {
+   console.log(`Server running on port ${port}`);
 });
