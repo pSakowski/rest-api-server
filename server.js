efc124059d2042480c6c46c6e306f31105555428
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-02-28 21:58:35 +0100

    update file server.js

--- server.js
+++ server.js
@@ -7,33 +7,8 @@ const mongoose = require('mongoose');
 const concertsRouter = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 const testimonialsRouter = require('./routes/testimonials.routes');
-// const notFoundRouter = require('./routes/404.routes');
 
 const port = process.env.PORT || 8000;
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/build')));
-
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
-
-// API endpoints
-app.use('/api/concerts', concertsRouter);
-app.use('/api/seats', seatsRoutes);
-app.use('/api/testimonials', testimonialsRouter);
-
-// Return the main index.html file for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build/index.html'));
-});
-
-// app.use(notFoundRouter);
 
 mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -42,8 +17,6 @@ db.once('open', () => {
   console.log('Connected to the database');
 });
 db.on('error', err => console.log('Error ' + err));
-
-console.log('Database connection successful');
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
@@ -57,4 +30,27 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('Socket disconnected');
   });
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/build')));
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Pass the `io` object to the request object for use in route handlers
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+// API endpoints
+app.use('/api/concerts', concertsRouter);
+app.use('/api/seats', seatsRoutes);
+app.use('/api/testimonials', testimonialsRouter);
+
+// Return the main index.html file for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
 });
