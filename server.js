344a1426c6bd33e463cdeb5e8a1ee76c46159280
Piotr Sakowski <p.sakowski1213@gmail.com>
2023-03-01 23:37:23 +0100

    fix server.js

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ db.once('open', () => {
 });
 db.on('error', err => console.log('Error ' + err));
 
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port: 3000');
 });
 
 const io = socket(server);
